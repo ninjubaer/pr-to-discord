e5f0baf2b19d454cdc2aa6ba4c7f4c48e2a1f589
ninju <74989292+ninjubaer@users.noreply.github.com>
2025-03-31 11:44:45 +0200

    edited embed

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@ const message = {
         title: payload?.pull_request?.title || 'No title',
         url: payload?.pull_request?.html_url || 'https://github.com/natroteam/natromacrodev/pulls',
         describtion: payload?.pull_request?.body || 'No description',
+        author: {
+            name: payload?.pull_request?.user?.login || 'No author',
+            url: (payload?.pull_request?.user?.login ? `https://github.com/${payload.pull_request.user.login}` : 'https://github.com/'),
+            icon_url: payload?.pull_request?.user?.avatar_url || "https://github.githubassets.com/favicons/favicon-dark.svg"
+        },
         timestamp: new Date().toISOString()
     }],
     components: [
