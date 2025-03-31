bda5e88c1d72ccdd4e2b08b9038d04b8577638b5
ninju <74989292+ninjubaer@users.noreply.github.com>
2025-03-31 11:33:33 +0200

    added log for payload

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import github from '@actions/github';
 import fetch from 'node-fetch';
 const payload = github.context.payload;
 
+console.log('Payload:', payload);
 const message = {
     embeds: [{
         title: payload?.pull_request?.title || 'No title',
